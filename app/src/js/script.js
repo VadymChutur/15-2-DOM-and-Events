a09739a3b03a32124c22e2f61226bc93730ce6db
Vadym Chutur <falloutgreck@gmail.com>
2024-09-04 16:46:51 +0300

    upd

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,5 +1,8 @@
 const titleEl = document.querySelector('.js-title');
 
 titleEl.textContent = 'Hello JS';
+titleEl.classList.add('title');
+titleEl.classList.remove('title');
+titleEl.classList.toggle('title');
 console.log(titleEl.textContent);
 console.dir(titleEl);
