4c13d0d8935a89a28d59857fbbbe89d3368e54db
Vadym Chutur <falloutgreck@gmail.com>
2024-09-09 16:08:36 +0300

    upd

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -4,5 +4,13 @@ titleEl.textContent = 'Hello JS';
 titleEl.classList.add('title');
 titleEl.classList.remove('title');
 titleEl.classList.toggle('title');
+titleEl.hidden = false;
 console.log(titleEl.textContent);
 console.dir(titleEl);
+console.dir(titleEl.getAttribute('data-id'));
+titleEl.setAttribute('hidden', 'true');
+titleEl.removeAttribute('hidden');
+
+const listEl = document.querySelector('.js-list');
+console.log(listEl.children[1]);
+console.dir(listEl);
